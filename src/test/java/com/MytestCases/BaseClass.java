1acae38c52de5d69f05381cc21a67bedf9465abf
Aman Mishra <am8964543@gmail.com>
2025-05-21 01:39:31 +0530

    new commit

--- src/test/java/com/MytestCases/BaseClass.java
+++ src/test/java/com/MytestCases/BaseClass.java
@@ -6,18 +6,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class BaseClass {
 	
-	public WebDriver driver;
-	@Test
+	public  WebDriver driver;
+	@Test(priority=2)
 	public void setUP() {
 		
-		driver= new ChromeDriver();
+			driver= new ChromeDriver();
+		
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+				
 		driver.findElement(By.xpath("//input[@name=\"username\"]")).sendKeys("Admin");
 		driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys("admin123");
 		driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
@@ -27,23 +31,35 @@ public class BaseClass {
 		String expectedTitle ="OrangeHRMgdg";
 		Assert.assertEquals(actualTitle, expectedTitle, "Title Mismatch");
 		
-		
-		
+
 		
 		
 
 	}
 	
-	@Test
-	public void add() {
-		System.out.println("Add user");
-		
-	}
-	@Test
-	public void add1() {
-		System.out.println("Add user1");
-		
-	}
-	
+		@Test(enabled=true, description ="Login test cases is passed",priority =1)
+		public void loginwithInvalidcredentials() {
+			driver= new ChromeDriver();
+			
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+					
+			driver.findElement(By.xpath("//input[@name=\"username\"]")).sendKeys("Admin");
+			driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys("admin1234");
+			driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+			
+            String expectedtext="Invalid credentials";
+            String actualtext=driver.findElement(By.xpath("//p[text()='Invalid credentials']")).getText();
+            System.out.println(actualtext);
+            Assert.assertEquals(expectedtext, actualtext);
+			driver.close();
+			
+			
+			
+			
+			
+			
+		}
 
 }
